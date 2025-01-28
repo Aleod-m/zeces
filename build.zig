02f3e0a2a95d2f7f1302ac402ca805a65c9f947a
AdrienDML <adriendml99@protonmail.com>
2025-01-28 03:49:57 +0100

    Implemented entity stores

--- build.zig
+++ build.zig
@@ -4,6 +4,7 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
+    const build_options = b.addOptions();
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -15,18 +16,21 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
+    const erasure_checks = b.option(
+        bool,
+        "erasure-checks",
+        "Enable type erasure checks.",
+    ) orelse (optimize == .Debug);
+    build_options.addOption(bool, "erasure_checks", erasure_checks);
+
+    const lib_source: std.Build.StaticLibraryOptions = .{
         .name = "zeces",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
-    });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
+    };
+    const lib = b.addStaticLibrary(lib_source);
+    lib.root_module.addOptions("build_options", build_options);
     b.installArtifact(lib);
 
     // Creates a step for unit testing. This only builds the test executable
@@ -36,12 +40,17 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    lib_unit_tests.root_module.addOptions("build_options", build_options);
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
+    // Check if the library and tests compile.
+    const check = b.step("check", "Check if zeces compiles.");
+    check.dependOn(&lib.step);
+    check.dependOn(&lib_unit_tests.step);
 }
--- src/root.zig
+++ src/root.zig
@@ -1,34 +1,9 @@
 const std = @import("std");
 const testing = std.testing;
-pub const world = @import("./world.zig");
-pub const component = @import("./component.zig");
-pub const system = @import("./system.zig");
+pub const world = @import("world.zig");
+pub const Entity = @import("Entity.zig");
+pub const component = @import("component.zig");
 
-test "basic add functionality" {
-    const Position = component.Define(struct {
-        const STORAGE: component.Storage = .Table;
-        const Data = struct {
-            x: f32,
-            y: f32,
-            z: f32,
-        };
-    });
-
-    const IterPos = system.Define(struct {
-        const Self = @This();
-        const SCHEDULE: world.Schedule = .Run;
-        const Positions: type = system.Query(struct { pos: Position });
-
-        fn run(position: Positions.Item) void {
-            _ = position;
-        }
-    });
-
-    var _world = world.Define(struct {
-        const COMPONENTS: [1]type = .{Position};
-        const SYSTEMS: [1]type = .{IterPos};
-    }){};
-    defer _world.shutdown();
-
-    try _world.run();
+test {
+    testing.refAllDecls(@This());
 }
